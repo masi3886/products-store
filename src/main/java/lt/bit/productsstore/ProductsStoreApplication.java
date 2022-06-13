e460cd73dd89d69a5e64b803f7308e328c6f38ea
masi <masi@tiatechnology.com>
2022-06-13 19:12:03 +0300

    Spring 17 - REST services

---
+++ src/main/java/lt/bit/productsstore/ProductsStoreApplication.java
@@ -0,0 +1,13 @@
+package lt.bit.productsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsStoreApplication.class, args);
+	}
+
+}
