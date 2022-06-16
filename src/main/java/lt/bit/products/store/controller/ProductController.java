4702425681e62e965c12c01476cb7754a57c3352
masi <masi@tiatechnology.com>
2022-06-16 19:01:17 +0300

    Spring 20

--- src/main/java/lt/bit/products/store/controller/ProductController.java
+++ src/main/java/lt/bit/products/store/controller/ProductController.java
@@ -3,13 +3,11 @@ package lt.bit.products.store.controller;
 import java.util.List;
 import lt.bit.products.store.model.Product;
 import lt.bit.products.store.service.ProductService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,9 +35,13 @@ class ProductController {
     return ResponseEntity.ok(product);
   }
 
-  @ResponseStatus(HttpStatus.NO_CONTENT)
   @DeleteMapping("/{id}")
-  void deleteProduct(@PathVariable("id") Integer productId) {
+  ResponseEntity<Void> deleteProduct(@PathVariable("id") Integer productId) {
+    Product product = service.findProduct(productId);
+    if (product == null) {
+      return ResponseEntity.notFound().build();
+    }
     service.deleteProduct(productId);
+    return ResponseEntity.noContent().build();
   }
 }
--- src/main/java/lt/bit/products/store/service/ProductRepository.java
+++ src/main/java/lt/bit/products/store/service/ProductRepository.java
@@ -2,9 +2,13 @@ package lt.bit.products.store.service;
 
 import lt.bit.products.store.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface ProductRepository extends JpaRepository<Product, Integer> {
 
+  @Query(value = "DELETE FROM store_items WHERE product_id = ?1", nativeQuery = true)
+    //@Query(value = "delete from ProductItem p where p.id = ?1")
+  void deleteStoreItems(Integer productId);
 }
